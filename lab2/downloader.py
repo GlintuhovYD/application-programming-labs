d697a3c08290efb3be70d41a74de9d0a8f4c5f45
VfW <163401307+GlintuhovYD@users.noreply.github.com>
2024-12-10 11:12:40 +0400

    Update downloader.py

--- lab2/downloader.py
+++ lab2/downloader.py
@@ -1,7 +1,11 @@
 from icrawler.builtin import GoogleImageCrawler
 import os
 
-def download_images(keyword, image_dir, num_images=10):
-    os.makedirs(image_dir, exist_ok=True)
-    google_crawler = GoogleImageCrawler(storage={'root_dir': image_dir})
-    google_crawler.crawl(keyword=keyword, max_num=num_images)
+def download_images(keyword: str, image_dir: str, num_images: int = 10) -> None:
+    try:
+        os.makedirs(image_dir, exist_ok=True)
+        google_crawler = GoogleImageCrawler(storage={'root_dir': image_dir})
+        google_crawler.crawl(keyword=keyword, max_num=num_images)
+    except Exception as e:
+        print(f"Ошибка при загрузке изображений: {e}")
+
