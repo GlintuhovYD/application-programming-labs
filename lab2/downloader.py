140e1f971fa711a25c37c7e68bc7fb39adbefdbe
VfW <163401307+GlintuhovYD@users.noreply.github.com>
2024-12-10 14:52:10 +0400

    Update downloader.py

--- lab2/downloader.py
+++ lab2/downloader.py
@@ -1,11 +1,24 @@
-from icrawler.builtin import GoogleImageCrawler
 import os
+from icrawler.builtin import GoogleImageCrawler
 
 def download_images(keyword: str, image_dir: str, num_images: int = 10) -> None:
+    """Downloads images using Google Image Crawler.
+
+    Creates a directory if it doesn't exist, then uses GoogleImageCrawler
+    to download images based on the provided keyword.  Handles exceptions
+    during the download process.
+
+    Args:
+        keyword: The keyword to search for images.
+        image_dir: The directory to save the downloaded images.
+        num_images: The maximum number of images to download (default is 10).
+
+    Returns:
+        None. Prints an error message if an exception occurs during download.
+    """
     try:
         os.makedirs(image_dir, exist_ok=True)
         google_crawler = GoogleImageCrawler(storage={'root_dir': image_dir})
         google_crawler.crawl(keyword=keyword, max_num=num_images)
     except Exception as e:
-        print(f"Ошибка при загрузке изображений: {e}")
-
+        raise Exception(f"Error downloading images: {e}") from e
